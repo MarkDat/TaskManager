09221dd71555f5fff1d72695abec115dfe7dcad2
MarkDat <mark.dat.code@gmail.com>
2021-12-19 19:11:05 +0700

    update db project

--- TM.Database/dbo/Tables/Phase.sql
+++ TM.Database/dbo/Tables/Phase.sql
@@ -2,6 +2,7 @@
     [Id]           INT           IDENTITY (1, 1) NOT NULL,
     [Name]         NVARCHAR (50) NULL,
     [AcceptMoveId] INT           NULL,
+    [Code]         VARCHAR (5)   NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
